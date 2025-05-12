36f4b039038a960bf4c18e8ed63b2acee6436cf0
Andrei Muzarau <95071115+amuzarau@users.noreply.github.com>
2025-05-13 00:12:23 +0800

    Add files via upload

---
+++ 16_Diamonds.py
@@ -0,0 +1,41 @@
+def main():
+    print('Diamonds')
+
+    # Display diamonds of sizes 0 through 6:
+    for diamond_size in range(0, 6):
+        display_outline_diamond(diamond_size)
+        print()
+        display_filled_diamond(diamond_size)
+        print()
+
+def display_outline_diamond(size):
+    # Display the top half of the diamond:
+    for i in range(size):
+        print(' ' * (size -  i - 1), end='')  # Left side space.
+        print('/', end='')  # Left side of diamond.
+        print(' ' * (i * 2), end='')  # Interior of diamond.
+        print('\\')  # Right side of diamond.
+
+    # Display the bottom half of the diamond:
+    for i in range(size):
+        print(' ' * i, end='')  # Left side space.
+        print('\\', end='')  # Left side of diamond.
+        print(' ' * ((size - i - 1) * 2), end='') # Interior of diamond.
+        print('/')  # Right side of diamond.
+
+def display_filled_diamond(size):
+    # Display the top half of the diamond:
+    for i in range(size):
+        print(' ' * (size - i - 1), end='')  # Left side space.
+        print('/' * (i + 1), end='')  # Left half of diamond.
+        print('\\' * (i + 1))  # Right half of diamond.
+    
+    # Display the bottom half of the diamond:
+    for i in range(size):
+        print(' ' * i, end='')  # Left side space
+        print('\\' * (size - i), end='')  # Left side of diamond.
+        print('/' * (size - i))  # Right side of diamond.
+
+# If this program was run (instead of imported), run the game:
+if __name__ == '__main__':
+    main()
